c18ba7469dac5f1dfc54879aae23093140f55504
july <julywhj@163.com>
2019-01-01 18:13:41 +0800

    基成WEB模块

---
+++ springcloud/springcloud-web/global-web-common/src/main/java/cn/org/july/web/common/annotation/DataSource.java
@@ -0,0 +1,23 @@
+package cn.org.july.web.common.annotation;
+
+import com.ruoyi.common.enums.DataSourceType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 自定义多数据源切换注解
+ * 
+ * @author ruoyi
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DataSource
+{
+    /**
+     * 切换数据源名称
+     */
+    DataSourceType value() default DataSourceType.MASTER;
+}
